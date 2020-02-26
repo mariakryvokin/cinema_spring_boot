a2be5688cbd49f72007bf7f99316bb75f4ffe377
Mariia_Kryvokin <mkryvokin@corelogic.com>
2020-02-26 17:56:34 +0200

    config dataSource from properties

--- src/main/java/app/config/RepositoryConfig.java
+++ src/main/java/app/config/RepositoryConfig.java
@@ -1,8 +1,11 @@
 package app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -31,13 +34,10 @@ public class RepositoryConfig {
     private Environment env;
 
     @Bean
+    @ConfigurationProperties(prefix="spring.datasource")
+    @Primary
     public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        return dataSource;
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
